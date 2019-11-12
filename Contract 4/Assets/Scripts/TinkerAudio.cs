3ea139e1e48e036fa8244355979d084d76a1e8b9
callum <metcalfewood@hotmail.com>
2019-11-12 10:59:45 +0000

    Added base unity audio generator for contract 4

---
+++ Contract 4/Assets/Scripts/TinkerAudio.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using NaughtyAttributes;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class TinkerAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private AudioClip outAudioClip;
+    public int frequency;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+
+    // Public APIs
+    public void PlayOutAudio()
+    {
+        outAudioClip = CreateToneAudioClip(frequency);
+        audioSource.PlayOneShot(outAudioClip);
+    }
+
+
+    public void StopAudio()
+    {
+        audioSource.Stop();
+    }
+
+
+    // Private 
+    private AudioClip CreateToneAudioClip(int frequency)
+    {
+        int sampleDurationSecs = 10;
+        int sampleRate = 44100;
+        int sampleLength = sampleRate * sampleDurationSecs;
+        float maxValue = 1f / 4f;
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+
+    private void IncreaseVolume()
+    {
+
+    }
+
+
+#if UNITY_EDITOR
+    //[Button("Save Wav file")]
+    private void SaveWavFile()
+    {
+        string path = EditorUtility.SaveFilePanel("Where do you want the wav file to go?", "", "", "wav");
+        var audioClip = CreateToneAudioClip(1500);
+        SaveWavUtil.Save(path, audioClip);
+    }
+#endif
+}
