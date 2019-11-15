852dc6cab24f0ef7976d53e6dccb6410dcf6d3a9
Callum Metcalfe <cm227781@falmouth.ac.uk>
2019-11-15 14:43:30 +0000

    Added a Play, Options and Quit button

--- Contract 4/Assets/Scripts/TinkerAudio.cs
+++ Contract 4/Assets/Scripts/TinkerAudio.cs
@@ -1,10 +1,9 @@
-﻿//using NaughtyAttributes;
+﻿
+
+//using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 
 public class TinkerAudio : MonoBehaviour
@@ -13,13 +12,18 @@ public class TinkerAudio : MonoBehaviour
     private AudioClip outAudioClip;
     private int currentFrequency;
     public int maxFrequency;
+    public TextMeshProUGUI frequencyText;
 
     [SerializeField]
     private Slider frequencySlider;
 
+    /// <summary>
+    /// Sets the starting frequency to the max frequency allowed
+    /// Whilst also setting the slider to be at its max value 
+    /// so that when the slider is moved it represents what the current frequency is
+    /// </summary>
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         currentFrequency = maxFrequency;
         frequencySlider.value = 1;
@@ -31,13 +35,21 @@ public class TinkerAudio : MonoBehaviour
         SliderFrequency();
     }
 
+    /// <summary>
+    /// Uses the slider to update the frequency variable
+    /// </summary>
     public void SliderFrequency()
     {
-        currentFrequency = Mathf.RoundToInt(frequencySlider.value * maxFrequency); 
+        currentFrequency = Mathf.RoundToInt(frequencySlider.value * maxFrequency);
+        frequencyText.text = "Frequency: " + currentFrequency.ToString();
     }
 
 
-    // Public APIs
+    /// <summary>
+    /// This calls the function CreateToneAudioClip, giving it a value for the frequency
+    /// After the audioclip has been generated it will then play the audio clip that was preduced
+    /// when the button is clicked
+    /// </summary>
     public void PlayOutAudio()
     {
         outAudioClip = CreateToneAudioClip(currentFrequency);
@@ -51,7 +63,16 @@ public class TinkerAudio : MonoBehaviour
     }
 
 
-    // Private 
+    /// <summary>
+    /// Within this funtion it sets the duration of the audio clip to be 0.2 seconds
+    /// It will then set the sample rate to be 44100
+    /// As the duation is set to a float it needs to be rounded when * by the sample rate to get the sample length
+    /// This is then used to get the levels of sound from a sine wave.
+    /// </summary>
+    /// <param name="frequency"></param>
+    /// <returns>
+    /// The audio clip to be used for the button
+    /// </returns>
     private AudioClip CreateToneAudioClip(int frequency)
     {
         float sampleDurationSecs = 0.2f;
@@ -78,14 +99,4 @@ public class TinkerAudio : MonoBehaviour
 
     }
 
-
-#if UNITY_EDITOR
-    //[Button("Save Wav file")]
-    private void SaveWavFile()
-    {
-        string path = EditorUtility.SaveFilePanel("Where do you want the wav file to go?", "", "", "wav");
-        var audioClip = CreateToneAudioClip(1500);
-        SaveWavUtil.Save(path, audioClip);
-    }
-#endif
 }
