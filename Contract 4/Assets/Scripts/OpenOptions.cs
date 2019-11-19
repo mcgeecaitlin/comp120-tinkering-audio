cd2f3a2483ca3acea13f8d5e07ba08d0d7b67f92
callum <callummgamedev@hotmail.com>
2019-11-19 10:47:18 +0000

    Added a Secondary scene where user can edit success and fail sounds Added a script to change between scenes

---
+++ Contract 4/Assets/Scripts/OpenOptions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenOptions : MonoBehaviour
+{
+    public void OptionsScene()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("MainMeny");
+    }
+}
