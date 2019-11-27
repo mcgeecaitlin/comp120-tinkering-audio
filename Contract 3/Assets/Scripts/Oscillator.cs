ba07838786e94395541b130a87980ae89803ad63
Caitlin <cm232388@falmouth.ac.uk>
2019-11-27 13:18:27 +0000

    *Initial commit of Contract 3 folder.

---
+++ Contract 3/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+	public double frequency = 440.0; // The frequency in Hertz if the tone that the oscillator will produce.
+	private double increment; // The amount of distance the wave will be moving in ech frame.
+	private double phrase; // Actual location on the wave.
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
