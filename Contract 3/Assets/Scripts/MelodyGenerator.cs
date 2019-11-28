0b1d346a0a304507ad87f17f2f6a68fa9065b27c
Caitlin <cm232388@falmouth.ac.uk>
2019-11-28 18:04:50 +0000

    *Started to implement a function to generate a random melody in MelodyGenerator.cs

---
+++ Contract 3/Assets/Scripts/MelodyGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MelodyGenerator : MonoBehaviour
+{
+    public double frequency = 440.0; // The frequency in Hertz if the tone that the oscillator will produce.
+    private double increment; // The amount of distance the wave will be moving in each frame.
+    private double phase; // Actual location on the wave.
+    private double sampling_frequency = 48000.0; // The default frequency of unity's audio engine.
+
+    public float gain; // The power or volume of the oscillator.
+    public float volume = 0.1f;
+
+    // Dictionary containing the frequencies of each major note in the musical scale.
+    private readonly Dictionary<string, int> frequencies = new Dictionary<string, int>()
+    {
+        { "A", 440 },
+        { "B", 494 },
+        { "C", 523 },
+        { "D", 587 },
+        { "E", 659 },
+        { "F", 698 },
+        { "G", 784 },
+        { "A", 880 }
+    };
+
+    private string[] noteValues = new string[] { "A", "B", "C", "D", "E", "F", "G", "A" };
+
+    private void Start()
+    {
+        RandomMelody(8);
+    }
+
+    // Randomly generating a melody.
+    private void RandomMelody(int randomAmount)
+    {
+        string[] pianoKeys = new string[randomAmount];
+        for (int i = 0; i < randomAmount; i++)
+        {
+            string randomString = noteValues[Random.Range(0, noteValues.Length)]; // Getting a random value from 0 to the size of the dictionary
+            pianoKeys[i] = frequencies.Keys.ElementAt(Random.Range(0, frequencies.Count));
+            Debug.Log(pianoKeys[i]);
+        }
+    }
+}
---
+++ Contract 3/Assets/Scripts/TinkeringAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TinkeringAudio : MonoBehaviour
+{
+    private AudioClip CreateToneAudioClip(int frequency)
+    {
+        int sampleDurationSecs = 5; // How long the clip is going to last.
+        int sampleRate = 44100;
+        int sampleLength = sampleRate * sampleDurationSecs; // How long the samples array will be.
+        float maxValue = 1f / 4f;
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+}
